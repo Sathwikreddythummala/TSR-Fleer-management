dd6c4bb62afbe2bbddea2ebf4ff40c881cf42302
Sathwikreddythummala <115337601+Sathwikreddythummala@users.noreply.github.com>
2025-09-29 11:29:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
-DB_USER = 'root'
-DB_PASS = 'Tsat@9392350848'    # <<-- change this
-DB_HOST = '127.0.0.1'  # as you provided
-DB_PORT = 3306
-DB_NAME = 'tsr_db'
+DB_USER = 'tsrfleet_user'
+DB_PASS = 'AETy1afH8aelRKs7OwurTcQYjIeyFtvk'    # <<-- change this
+DB_HOST = 'dpg-d3d1vdq4d50c73cvvvrg-a'  # as you provided
+DB_PORT = 5432
+DB_NAME = 'tsrfleet'
 SECRET_KEY = 'Tsat@9392350848'
